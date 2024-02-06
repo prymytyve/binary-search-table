dad8fa374afcf303d331ede035ff0363693ce9c0
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-02-06 02:24:36 -0500

    BuildTree method fix Removes the inner recursive function. Instead uses call method to recursively call BuildTree method.

--- mode_classTree.mjs
+++ mode_classTree.mjs
@@ -5,20 +5,31 @@ export default class Tree {
     this.root = this.buildTree(array);
   }
 
-  buildTree = (array) => {
-    let rootNode = null;
-    const buildRec = (arr) => {
-      if (arr.length <= 0) return null;
-      let mid = Math.floor(arr.length / 2);
-      let left = arr.splice(0, mid);
-      let midVal = arr.shift();
-      let right = arr;
-      let node = new Node(midVal);
-      node.left = buildRec(left);
-      node.right = buildRec(right);
-      return node;
-    };
-    rootNode = buildRec(array);
-    return rootNode;
+  //   buildTree = (array) => {
+  //     let rootNode = null;
+  //     const buildRec = (arr) => {
+  //       if (arr.length <= 0) return null;
+  //       let mid = Math.floor(arr.length / 2);
+  //       let left = arr.splice(0, mid);
+  //       let midVal = arr.shift();
+  //       let right = arr;
+  //       let node = new Node(midVal);
+  //       node.left = buildRec(left);
+  //       node.right = buildRec(right);
+  //       return node;
+  //     };
+  //     rootNode = buildRec(array);
+  //     return rootNode;
+  //   };
+  buildTree = (arr) => {
+    if (arr.length <= 0) return null;
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.splice(0, mid);
+    let midVal = arr.shift();
+    let right = arr;
+    let node = new Node(midVal);
+    node.left = this.buildTree.call(this, left);
+    node.right = this.buildTree.call(this, right);
+    return node;
   };
 }
