6c7f44fa815b88eb29d4f14ae050723e37ceacac
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-02-06 02:26:40 -0500

    Removes old buildTree method

--- mode_classTree.mjs
+++ mode_classTree.mjs
@@ -5,22 +5,6 @@ export default class Tree {
     this.root = this.buildTree(array);
   }
 
-  //   buildTree = (array) => {
-  //     let rootNode = null;
-  //     const buildRec = (arr) => {
-  //       if (arr.length <= 0) return null;
-  //       let mid = Math.floor(arr.length / 2);
-  //       let left = arr.splice(0, mid);
-  //       let midVal = arr.shift();
-  //       let right = arr;
-  //       let node = new Node(midVal);
-  //       node.left = buildRec(left);
-  //       node.right = buildRec(right);
-  //       return node;
-  //     };
-  //     rootNode = buildRec(array);
-  //     return rootNode;
-  //   };
   buildTree = (arr) => {
     if (arr.length <= 0) return null;
     let mid = Math.floor(arr.length / 2);
