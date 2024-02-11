f9c5a015139d1705d38ddee591b2b4ba18b10b5e
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-02-10 22:41:30 -0500

    Adds delete method

--- mainScript.mjs
+++ mainScript.mjs
@@ -1,8 +1,12 @@
 import Tree from "./mod_classTree.mjs";
 
 const array = [21, 6513, 5, 33, 1, 5, 2, 4, 3, 1000, 7, 51, 6, 0];
+const array1 = [2, 6, 7];
+const array2 = [6, 3];
+const array3 = [6];
 const test = new Tree(array);
 
-//console.log(test.root);
-test.insert(8);
+// console.log(test.root);
+// test.insert(8);
+test.delete();
 test.prettyPrint();
--- mod_classTree.mjs
+++ mod_classTree.mjs
@@ -43,33 +43,6 @@ export default class Tree {
     }
   };
 
-  insertFail = (val) => {
-    let temp = this.root;
-    while (temp !== null) {
-      if (val < temp.data) {
-        temp = temp.left;
-      } else if (val > temp.data) {
-        temp = temp.right;
-      } else if (val === temp.data) {
-        console.log("Dupes are not permitted!");
-        return;
-      }
-    }
-    temp = new Node(val);
-  };
-
-  // insert = (val, node = this.root) => {
-  //   if (node === null) {
-  //     node = new Node(val);
-  //   } else if (val === node.data) {
-  //     console.log("Beware of Dupes!");
-  //   } else {
-  //     let temp;
-  //     val < node.data ? (temp = node.left) : (temp = node.right);
-  //     this.insert.call(this, val, temp);
-  //   }
-  // };
-
   insert = (val, node = this.root) => {
     if (val === node.data) {
       console.log("Beware of Dupes!");
@@ -83,6 +56,76 @@ export default class Tree {
         : this.insert.call(this, val, node.right);
     } else if (val === undefined || typeof val !== "number") {
       console.log("Missing value or incorrect value");
+    }
+  };
+
+  traversal = (val, node = this.root) => {
+    if (val < node.data && node.left !== null) {
+      return this.traversal.call(this, val, node.left);
+    } else if (val > node.data && node.right !== null) {
+      return this.traversal.call(this, val, node.right);
+    } else if (val === node.data) {
+      return node;
+    } else {
+      console.log("Value not Found");
+      return;
+    }
+  };
+
+  next = (val, node) => {
+    val < node.data ? (node = node.left) : (node = node.right);
+    return node;
+  };
+
+  delete = (val, node = this.root, nodeParent) => {
+    //node w/ 2 children...works for root too...............................................
+    if (val === undefined) return;
+    if (val === node.data && node.right !== null && node.left !== null) {
+      let parent = node;
+      let child = node.right;
+      while (child.left !== null) {
+        parent = child;
+        child = child.left;
+      }
+      node._data = child.data;
+      parent.left.data === child.data
+        ? (parent.left = null)
+        : (parent.right = null);
+    } //node for parent w/ 1 child........................................................
+    else if (val === node.data && (node.right !== null || node.left !== null)) {
+      if (this.root.data === val) {
+        let existingVal = undefined;
+        this.root.left.data !== null
+          ? (existingVal = this.root.left.data)
+          : (existingVal = this.root.right.data);
+        this.root._data = existingVal;
+        this.root.left !== null
+          ? (this.root.left = null)
+          : (this.root.right = null);
+      } else {
+        let existingNode = undefined;
+        node.left.data
+          ? (existingNode = node.left)
+          : (existingNode = node.right);
+        nodeParent.left.data === val
+          ? (nodeParent.left = existingNode)
+          : (nodeParent.right = existingNode);
+      }
+    } //leaf node...........................................................................
+    else if (val === node.data && node.right === null && node.left === null) {
+      if (this.root.data === val) {
+        this.root = null;
+      } else {
+        nodeParent.left.data === val
+          ? (nodeParent.left = null)
+          : (nodeParent.right = null);
+      }
+    }
+    //recursion function....................................................................
+    else if (val !== node.data) {
+      let parent = node;
+      let child = this.next.call(this, val, node);
+      this.delete.call(this, val, child, parent);
     }
   };
 }
